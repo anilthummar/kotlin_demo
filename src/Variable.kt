1c4044e7e6f8a7acf5d012f60750dcc38f8f1255
Anil Thummar <>
2022-02-22 13:23:25 +0530

    init

---
+++ src/Variable.kt
@@ -0,0 +1,12 @@
+
+fun main() {
+
+    var temp:Double = -3.55
+    var yes_no:Boolean=true
+    var name :String ="My Name is Kotlin"
+    var singleChar:Char='A'
+
+    println("My_Variables : $name $singleChar $temp $yes_no")
+
+
+}

b3297a145754290234751ca0f40e430fe7c6eac5
Anil Thummar <>
2022-02-22 16:46:45 +0530

    init

--- src/Variable.kt
+++ src/Variable.kt
@@ -1,10 +1,10 @@
 
 fun main() {
 
-    var temp:Double = -3.55
-    var yes_no:Boolean=true
-    var name :String ="My Name is Kotlin"
-    var singleChar:Char='A'
+    val temp:Double = -3.55
+    val yes_no:Boolean=true
+    val name :String ="My Name is Kotlin"
+    val singleChar:Char='A'
 
     println("My_Variables : $name $singleChar $temp $yes_no")
 
--- src/kotlin.kt
+++ src/kotlin.kt
@@ -2,6 +2,6 @@ fun main() {
 
     println("its static type lang - Type check at compile time")
     println("its Object oriented lang")
-    println("its Functional lang - lamda and all ")
+    println("its Functional lang - lambda and all ")
 
 }
